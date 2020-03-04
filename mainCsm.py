4da92f727a43eecd6de8a9a09f9455f48978591a
Akhil Dasika <akhildasika@amdg.rockhursths.edu>
2020-03-04 08:31:19 -0600

    CSM main

---
+++ mainCsm.py
@@ -0,0 +1,19 @@
+from CSM import Deck
+
+deck1 = Deck()
+deck1.create()
+c = int(0)
+d = int(0)
+
+while(d == 0):
+    b = int(input("1 to print"))
+    if(b == 1):
+        deck1.shuffle()
+        deck1.pt()
+
+
+    
+
+
+
+
