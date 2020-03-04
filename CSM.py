4dd04c2d3f974cb39157ced7da70b281f4cb37ec
Akhil Dasika <akhildasika@amdg.rockhursths.edu>
2020-03-04 08:30:43 -0600

    csm

--- CSM.py
+++ CSM.py
@@ -12,13 +12,17 @@ class Deck(object):
     
     def create(self):
         self.cards = []
-        for i in range(6):
+        for a in range(6):
             for i in range(4):
                 for j in range(0,14):
-                    self.cards.append(Card(i, j))
+                       self.cards.append(Card(i, j))
 
     def shuffle(self):
         n = len(self.cards)
         for i in range(n-1,0,-1): 
             j = random.randint(0,i+1) 
             self.cards[i],self.cards[j] = self.cards[j],self.cards[i]
+
+
+    def pt(self):
+        print(self)
