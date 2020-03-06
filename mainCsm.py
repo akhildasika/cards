a4c0d1739b0d9af86e3221dd12c927a78133229a
Akhil Dasika <akhildasika@amdg.rockhursths.edu>
2020-03-06 08:36:19 -0600

    main

--- mainCsm.py
+++ mainCsm.py
@@ -1,19 +1,25 @@
 from CSM import Deck
-
-deck1 = Deck()
-deck1.create()
+from cardcountClass import Card
+decks = Deck()
+x = int(input("how many decks: "))
+for i in range(x):
+    deck1 = Deck()
+    deck1.create()
+    for i in range(4):
+            for j in range(0,14):
+                    decks.cards.append(Card(i, j))
 c = int(0)
 d = int(0)
 
 while(d == 0):
     b = int(input("1 to print: "))
     if(b == 1):
-        deck1.shuffle()
-        deck1.pt()
+        decks.shuffle()
+        decks.pt()
         b = 0
         a = int(input("2 to draw first card, 3 to reshuffle: "))
         if(a == 2):
-            print(deck1.deal)
+            print(decks.deal)
             a = 0
         else:
             a = 0
