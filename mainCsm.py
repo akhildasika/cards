c69b77fe92cd7d0ec6ac18be5dee8303e79656de
Akhil Dasika <akhildasika@amdg.rockhursths.edu>
2020-03-06 08:20:18 -0600

    main

--- mainCsm.py
+++ mainCsm.py
@@ -6,10 +6,17 @@ c = int(0)
 d = int(0)
 
 while(d == 0):
-    b = int(input("1 to print"))
+    b = int(input("1 to print: "))
     if(b == 1):
         deck1.shuffle()
         deck1.pt()
+        b = 0
+        a = int(input("2 to draw first card, 3 to reshuffle: "))
+        if(a == 2):
+            print(deck1.deal)
+            a = 0
+                
+                
 
 
     
